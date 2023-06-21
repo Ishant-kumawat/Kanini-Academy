46fbf6d550518503708ab8a5b99fafb8ff45f010
Thrupthi M N <thrupthi.kanini@gmail.com>
2023-06-21 11:55:50 +0530

    Add files via upload

---
+++ component1/component1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import { Continue1Component } from '../continue1/continue1.component';
+
+@Component({
+  selector: 'app-component1',
+  templateUrl: './component1.component.html',
+  styleUrls: ['./component1.component.css']
+})
+export class Component1Component {
+      
+}
