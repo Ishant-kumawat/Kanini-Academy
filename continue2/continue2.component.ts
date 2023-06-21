9c8838af2663ba181be6279285cb1ca668886df6
Thrupthi M N <thrupthi.kanini@gmail.com>
2023-06-21 14:56:31 +0530

    Add files via upload

---
+++ continue2/continue2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-continue2',
+  templateUrl: './continue2.component.html',
+  styleUrls: ['./continue2.component.css']
+})
+export class Continue2Component {
+
+}
